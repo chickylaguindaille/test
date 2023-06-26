ecfe8aca08ce645e81c4aa68a168b2b3b829d1c8
Leroy <geti.leroy@gmail.com>
2023-06-26 12:43:38 +0200

    save

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitfab291231dd0706cbc315fedde987c35::getLoader();
+return ComposerAutoloaderInit18928b446e853875a9815a0edecd0f57::getLoader();
